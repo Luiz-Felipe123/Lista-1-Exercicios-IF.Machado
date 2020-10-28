f68c1cfcf2f6c69e5962259f052527f4d3e53b25
Luiz-Felipe123 <70417435+Luiz-Felipe123@users.noreply.github.com>
2020-10-28 19:03:12 -0300

    Update 21- Fibbonaci.c

--- 21- Fibbonaci.c
+++ 21- Fibbonaci.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
-/*A série de Fibonacci é formada pela sequência:
+/*A sÃ©rie de Fibonacci Ã© formada pela sequÃªncia:
 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, ...
-Escreva um programa que gere a série de FIBONACCI até o N-ésimo termo informado pelo usuário.
+Escreva um programa que gere a sÃ©rie de FIBONACCI atÃ© o N-Ã©simo termo informado pelo usuÃ¡rio.
 */
 
 int main()
 {
     int i, n, soma;
-    int f1 = 1, f2 = 1; // f1 e f2 representam respectivamente o primeiro e segundo termo da sequência
+    int f1 = 1, f2 = 1;
     scanf("%d", &n);
-    printf("%d %d ", f1, f2); // Inicio da sequência
-    for(i = 3; i <= n; i++) // Como os dois primeiros termos ja foram iniciados(vide f1, f2), o laço começa com 3(terceiro termo)
+    printf("%d %d ", f1, f2); // Inicio da sequÃªncia
+    for(i = 3; i <= n; i++)
     {
-        soma = f1 + f2; // Soma simples do f1 + f2
-        f1 = f2; // F1 recebe F2, exemplo: se f2 = 2, f1 ficará igual a 2
-        f2 = soma; // F2 recebe soma, exemplo: se soma for igual a 3, f2 recebe soma e volta o loop somando os novos valores de f1 e f2
+        soma = f1 + f2; 
+        f1 = f2; 
+        f2 = soma; 
         printf("%d ", f2);
-    } // Fim do laço
+    } // Fim do laÃ§o
 return 0;
 }
